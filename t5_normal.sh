633c1a6efc57516e1f0c2222b3ea9200a04e4a0c
donggcao18 <caovdongg18@gmail.com>
2026-03-10 11:16:46 +0700

    update training scripts for t5 model

--- t5_normal.sh
+++ t5_normal.sh
@@ -6,74 +6,79 @@
 #SBATCH -N 1                           
 #SBATCH -t 20:00:00   
 #SBATCH --mem 128G 
-#SBATCH --gres=gpu:a100-sxm4-80gb:1
-
+#SBATCH --gres=gpu:a100-sxm4-80gb:2
+export HF_HOME=./.cache
+export HF_DATASETS_CACHE=./.cache
 fuser -k /dev/nvidia*
 
 export CUDA_DEVICE_ORDER="PCI_BUS_ID"
 export TORCH_DISTRIBUTED_DEBUG=DETAIL
-port=$(shuf -i25000-30000 -n1)  
+export CUDA_VISIBLE_DEVICES=0
+port=$(shuf -i25000-30000 -n1)
+DS_CONFIG="configs/ds_configs/stage2_without_offload.config"
 
-python3 src/run_t5_new.py \
+
+# python src/run_t5_new.py \
+#    --do_train \
+#    --do_predict \
+#    --predict_with_generate \
+#    --model_name_or_path Salesforce/codet5p-770m \
+#    --data_dir CODETASK_Benchmark \
+#    --task_order CONCODE,CodeTrans,CodeSearchNet,BFP,KodCode,RunBugRun,TheVault_Csharp,CoST \
+#    --task_config_dir configs/CodeTask/CONCODE \
+#    --output_dir logs_and_outputs/test_t5_codetask_train_top_1_test_top_1_train_top_p_-1.0_test_top_p_-1.0/outputs/1-CONCODE \
+#    --per_device_train_batch_size 32 \
+#    --per_device_eval_batch_size 32 \
+#    --gradient_accumulation_steps 1 \
+#    --learning_rate 3e-04 \
+#    --attn_lr 0.0 \
+#    --num_train_epochs 5 \
+#    --run_name test_t5_codetask_train_top_1_test_top_1_train_top_p_-1.0_test_top_p_-1.0 \
+#    --distances_temperature 1.0 \
+#    --distances_way L2 \
+#    --max_source_length 320 \
+#    --max_target_length 150 \
+#    --generation_max_length 150 \
+#    --add_task_name False \
+#    --add_dataset_name False \
+#    --overwrite_output_dir \
+#    --overwrite_cache \
+#    --lr_scheduler_type constant \
+#    --warmup_steps 0 \
+#    --logging_strategy steps \
+#    --logging_steps 100 \
+#    --metric_for_best_model eval_rougeL \
+#    --evaluation_strategy steps \
+#    --save_strategy steps \
+#    --save_total_limit 1 \
+#    --lora_r 8 \
+#    --lora_alpha 32 \
+#    --lora_dropout 0.0 \
+#    --load_best_model_at_end \
+#    --data_replay_freq -1 \
+#    --replay_after_n_epoch 0 \
+#    --kl_ratio 1 \
+#    --attn_temperature 1 \
+#    --train_key_weight_top 1 \
+#    --test_key_weight_top 1 \
+#    --train_key_weight_top_p -1.0 \
+#    --test_key_weight_top_p -1.0 \
+#    --successor N \
+#    --bf16 \
+#    # --deepspeed $DS_CONFIG \
+
+# rm -rf logs_and_outputs/test_t5_codetask_train_top_1_test_top_1_train_top_p_-1.0_test_top_p_-1.0/outputs/1-CONCODE/checkpoint*
+
+
+python src/run_t5_new.py \
    --do_train \
    --do_predict \
    --predict_with_generate \
    --model_name_or_path Salesforce/codet5p-770m \
-   --data_dir CODETASK_Benchmark \
-   --task_order CONCODE,CodeTrans,CodeSearchNet,BFP \
-   --task_config_dir configs/CodeTask/CONCODE \
-   --output_dir logs_and_outputs/test_t5_codetask_train_top_1_test_top_1_train_top_p_-1.0_test_top_p_-1.0/outputs/1-CONCODE \
-   --per_device_train_batch_size 128 \
-   --per_device_eval_batch_size 32 \
-   --gradient_accumulation_steps 1 \
-   --learning_rate 3e-04 \
-   --attn_lr 0.0 \
-   --num_train_epochs 5 \
-   --bf16 \
-   --run_name test_t5_codetask_train_top_1_test_top_1_train_top_p_-1.0_test_top_p_-1.0 \
-   --distances_temperature 1.0 \
-   --distances_way L2 \
-   --max_source_length 320 \
-   --max_target_length 150 \
-   --generation_max_length 150 \
-   --add_task_name False \
-   --add_dataset_name False \
-   --overwrite_output_dir \
-   --overwrite_cache \
-   --lr_scheduler_type constant \
-   --warmup_steps 0 \
-   --logging_strategy steps \
-   --logging_steps 10 \
-   --metric_for_best_model eval_rougeL \
-   --evaluation_strategy steps \
-   --save_strategy steps \
-   --save_total_limit 1 \
-   --lora_r 8 \
-   --lora_alpha 32 \
-   --lora_dropout 0.0 \
-   --load_best_model_at_end \
-   --data_replay_freq -1 \
-   --replay_after_n_epoch 0 \
-   --kl_ratio 1 \
-   --attn_temperature 1 \
-   --train_key_weight_top 1 \
-   --test_key_weight_top 1 \
-   --train_key_weight_top_p -1.0 \
-   --test_key_weight_top_p -1.0 \
-   --successor N 
-
-rm -rf logs_and_outputs/test_t5_codetask_train_top_1_test_top_1_train_top_p_-1.0_test_top_p_-1.0/outputs/1-CONCODE/checkpoint*
-
-
-python3 src/run_t5_new.py \
-   --do_train \
-   --do_predict \
-   --predict_with_generate \
-   --model_name_or_path Salesforce/codet5-base \
    --previous_lora_path logs_and_outputs/test_t5_codetask_train_top_1_test_top_1_train_top_p_-1.0_test_top_p_-1.0/outputs/1-CONCODE/saved_weights \
    --previous_lora_distribution_path logs_and_outputs/test_t5_codetask_train_top_1_test_top_1_train_top_p_-1.0_test_top_p_-1.0/outputs/1-CONCODE/saved_weights \
    --data_dir CODETASK_Benchmark \
-   --task_order CONCODE,CodeTrans,CodeSearchNet,BFP \
+   --task_order CONCODE,CodeTrans,CodeSearchNet,BFP,KodCode,RunBugRun,TheVault_Csharp,CoST \
    --gen_data_dir generated_data/lora_gen_superni_llama \
    --task_config_dir configs/CodeTask/CodeTrans \
    --output_dir logs_and_outputs/test_t5_codetask_train_top_1_test_top_1_train_top_p_-1.0_test_top_p_-1.0/outputs/2-CodeTrans \
@@ -114,21 +119,22 @@ python3 src/run_t5_new.py \
    --test_key_weight_top 1 \
    --train_key_weight_top_p -1.0 \
    --test_key_weight_top_p -1.0 \
-   --successor N
+   --successor N \
+   # --deepspeed $DS_CONFIG \
 
 rm -rf logs_and_outputs/test_t5_codetask_train_top_1_test_top_1_train_top_p_-1.0_test_top_p_-1.0/outputs/2-CodeTrans/checkpoint*
 
 
 
-python3 src/run_t5_new.py \
+python src/run_t5_new.py \
    --do_train \
    --do_predict \
    --predict_with_generate \
-   --model_name_or_path Salesforce/codet5-base \
+   --model_name_or_path Salesforce/codet5p-770m \
    --previous_lora_path logs_and_outputs/test_t5_codetask_train_top_1_test_top_1_train_top_p_-1.0_test_top_p_-1.0/outputs/1-CONCODE/saved_weights,logs_and_outputs/test_t5_codetask_train_top_1_test_top_1_train_top_p_-1.0_test_top_p_-1.0/outputs/2-CodeTrans/saved_weights \
    --previous_lora_distribution_path logs_and_outputs/test_t5_codetask_train_top_1_test_top_1_train_top_p_-1.0_test_top_p_-1.0/outputs/1-CONCODE/saved_weights,logs_and_outputs/test_t5_codetask_train_top_1_test_top_1_train_top_p_-1.0_test_top_p_-1.0/outputs/2-CodeTrans/saved_weights \
    --data_dir CODETASK_Benchmark \
-   --task_order CONCODE,CodeTrans,CodeSearchNet,BFP \
+   --task_order CONCODE,CodeTrans,CodeSearchNet,BFP,KodCode,RunBugRun,TheVault_Csharp,CoST \
    --gen_data_dir generated_data/lora_gen_superni_llama \
    --task_config_dir configs/CodeTask/CodeSearchNet \
    --output_dir logs_and_outputs/test_t5_codetask_train_top_1_test_top_1_train_top_p_-1.0_test_top_p_-1.0/outputs/3-CodeSearchNet \
@@ -169,21 +175,22 @@ python3 src/run_t5_new.py \
    --test_key_weight_top 1 \
    --train_key_weight_top_p -1.0 \
    --test_key_weight_top_p -1.0 \
-   --successor N
+   --successor N \
+   # --deepspeed $DS_CONFIG \
 
 rm -rf logs_and_outputs/test_t5_codetask_train_top_1_test_top_1_train_top_p_-1.0_test_top_p_-1.0/outputs/3-CodeSearchNet/checkpoint*
 
 
 
-python3 src/run_t5_new.py \
+python src/run_t5_new.py \
    --do_train \
    --do_predict \
    --predict_with_generate \
-   --model_name_or_path Salesforce/codet5-base \
+   --model_name_or_path Salesforce/codet5p-770m \
    --previous_lora_path logs_and_outputs/test_t5_codetask_train_top_1_test_top_1_train_top_p_-1.0_test_top_p_-1.0/outputs/1-CONCODE/saved_weights,logs_and_outputs/test_t5_codetask_train_top_1_test_top_1_train_top_p_-1.0_test_top_p_-1.0/outputs/2-CodeTrans/saved_weights,logs_and_outputs/test_t5_codetask_train_top_1_test_top_1_train_top_p_-1.0_test_top_p_-1.0/outputs/3-CodeSearchNet/saved_weights \
    --previous_lora_distribution_path logs_and_outputs/test_t5_codetask_train_top_1_test_top_1_train_top_p_-1.0_test_top_p_-1.0/outputs/1-CONCODE/saved_weights,logs_and_outputs/test_t5_codetask_train_top_1_test_top_1_train_top_p_-1.0_test_top_p_-1.0/outputs/2-CodeTrans/saved_weights,logs_and_outputs/test_t5_codetask_train_top_1_test_top_1_train_top_p_-1.0_test_top_p_-1.0/outputs/3-CodeSearchNet/saved_weights \
    --data_dir CODETASK_Benchmark \
-   --task_order CONCODE,CodeTrans,CodeSearchNet,BFP \
+   --task_order CONCODE,CodeTrans,CodeSearchNet,BFP,KodCode,RunBugRun,TheVault_Csharp,CoST \
    --gen_data_dir generated_data/lora_gen_superni_llama \
    --task_config_dir configs/CodeTask/BFP \
    --output_dir logs_and_outputs/test_t5_codetask_train_top_1_test_top_1_train_top_p_-1.0_test_top_p_-1.0/outputs/4-BFP \
@@ -224,7 +231,8 @@ python3 src/run_t5_new.py \
    --test_key_weight_top 1 \
    --train_key_weight_top_p -1.0 \
    --test_key_weight_top_p -1.0 \
-   --successor N
+   --successor N \
+      # --deepspeed $DS_CONFIG 
 
 rm -rf logs_and_outputs/test_t5_codetask_train_top_1_test_top_1_train_top_p_-1.0_test_top_p_-1.0/outputs/4-BFP/checkpoint*
 
@@ -236,11 +244,11 @@ rm -rf logs_and_outputs/test_t5_codetask_train_top_1_test_top_1_train_top_p_-1.0
 # python3 src/run_t5_new_eval.py \
 #    --do_predict \
 #    --predict_with_generate \
-#    --model_name_or_path Salesforce/codet5-base \
+#    --model_name_or_path Salesforce/codet5p-770m \
 #    --previous_lora_path logs_and_outputs/test_t5_codetask_train_top_1_test_top_1_train_top_p_-1.0_test_top_p_-1.0/outputs/1-CONCODE/saved_weights,logs_and_outputs/test_t5_codetask_train_top_1_test_top_1_train_top_p_-1.0_test_top_p_-1.0/outputs/2-CodeTrans/saved_weights,logs_and_outputs/test_t5_codetask_train_top_1_test_top_1_train_top_p_-1.0_test_top_p_-1.0/outputs/3-CodeSearchNet/saved_weights,logs_and_outputs/test_t5_codetask_train_top_1_test_top_1_train_top_p_-1.0_test_top_p_-1.0/outputs/4-BFP/saved_weights \
 #    --previous_lora_distribution_path logs_and_outputs/test_t5_codetask_train_top_1_test_top_1_train_top_p_-1.0_test_top_p_-1.0/outputs/1-CONCODE/saved_weights,logs_and_outputs/test_t5_codetask_train_top_1_test_top_1_train_top_p_-1.0_test_top_p_-1.0/outputs/2-CodeTrans/saved_weights,logs_and_outputs/test_t5_codetask_train_top_1_test_top_1_train_top_p_-1.0_test_top_p_-1.0/outputs/3-CodeSearchNet/saved_weights,logs_and_outputs/test_t5_codetask_train_top_1_test_top_1_train_top_p_-1.0_test_top_p_-1.0/outputs/4-BFP/saved_weights \
 #    --data_dir CODETASK_Benchmark \
-#    --task_order CONCODE,CodeTrans,CodeSearchNet,BFP \
+#    --task_order CONCODE,CodeTrans,CodeSearchNet,BFP,KodCode,RunBugRun,TheVault_Csharp,CoST \
 #    --gen_data_dir generated_data/lora_gen_superni_llama \
 #    --task_config_dir configs/CodeTask/BFP \
 #    --output_dir logs_and_outputs/test_t5_codetask_train_top_1_test_top_1_train_top_p_-1.0_test_top_p_-1.0/outputs/15-task875_emotion_classification \
