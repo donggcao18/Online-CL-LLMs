f7b4f17aef92bad7ae5f03edeab83b6c45fc67dc
donggcao18 <141107494+donggcao18@users.noreply.github.com>
2026-03-11 14:40:01 +0700

    Update run_t5_new.py

--- src/run_t5_new.py
+++ src/run_t5_new.py
@@ -419,7 +419,7 @@ def main():
 
     if cur_task in task_order:
         cur_task_id = task_order.index(cur_task)
-        if training_args.do_predict and !training_args.do_train:
+        if training_args.do_predict and not training_args.do_train:
             cur_task_id +=1
     else:
         cur_task_id=14
