8580fbb32059fe9a635ec6e4eb0285a69b9f3799
donggcao18 <caovdongg18@gmail.com>
2026-04-19 17:14:15 +0700

    +

--- t5_normal_full.sh
+++ t5_normal_full.sh
@@ -66,6 +66,8 @@ python src/run_t5_new.py \
    --successor N \
    --bf16 \
    --max_train_samples 100 \
+   --max_eval_samples 100 \
+   --max_predict_samples 100 \
    # --deepspeed $DS_CONFIG \
 
 rm -rf logs_and_outputs/test_t5_codetask_train_top_1_test_top_1_train_top_p_-1.0_test_top_p_-1.0/outputs/1-CONCODE/checkpoint*
@@ -122,6 +124,8 @@ python src/run_t5_new.py \
    --test_key_weight_top_p -1.0 \
    --successor N \
    --max_train_samples 100 \
+   --max_eval_samples 100 \
+   --max_predict_samples 100 \
    # --deepspeed $DS_CONFIG \
 
 rm -rf logs_and_outputs/test_t5_codetask_train_top_1_test_top_1_train_top_p_-1.0_test_top_p_-1.0/outputs/2-CodeTrans/checkpoint*
