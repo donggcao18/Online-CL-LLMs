1cb0fe23eda9e8e26aa6a46b881283bdfb06c68c
donggcao18 <caovdongg18@gmail.com>
2026-04-20 02:17:40 +0700

    +

--- t5_normal_full.sh
+++ t5_normal_full.sh
@@ -27,9 +27,9 @@ DS_CONFIG="configs/ds_configs/stage2_without_offload.config"
 #    --task_order CONCODE,CodeTrans,CodeSearchNet,BFP,KodCode,RunBugRun,TheVault_Csharp,CoST \
 #    --task_config_dir configs/CodeTask/CONCODE \
 #    --output_dir logs_and_outputs/test_t5_codetask_train_top_1_test_top_1_train_top_p_-1.0_test_top_p_-1.0/outputs/1-CONCODE \
-#    --per_device_train_batch_size 8 \
+#    --per_device_train_batch_size 32 \
 #    --per_device_eval_batch_size 16 \
-#    --gradient_accumulation_steps 4 \
+#    --gradient_accumulation_steps 1 \
 #    --learning_rate 3e-04 \
 #    --attn_lr 0.0 \
 #    --num_train_epochs 3 \
@@ -82,9 +82,9 @@ DS_CONFIG="configs/ds_configs/stage2_without_offload.config"
 #    --gen_data_dir generated_data/lora_gen_superni_llama \
 #    --task_config_dir configs/CodeTask/CodeTrans \
 #    --output_dir logs_and_outputs/test_t5_codetask_train_top_1_test_top_1_train_top_p_-1.0_test_top_p_-1.0/outputs/2-CodeTrans \
-#    --per_device_train_batch_size 8 \
+#    --per_device_train_batch_size 32 \
 #    --per_device_eval_batch_size 16 \
-#    --gradient_accumulation_steps 4 \
+#    --gradient_accumulation_steps 1 \
 #    --learning_rate 3e-04 \
 #    --attn_lr 0.0 \
 #    --num_train_epochs 3 \
@@ -138,9 +138,9 @@ DS_CONFIG="configs/ds_configs/stage2_without_offload.config"
 #    --gen_data_dir generated_data/lora_gen_superni_llama \
 #    --task_config_dir configs/CodeTask/CodeSearchNet \
 #    --output_dir logs_and_outputs/test_t5_codetask_train_top_1_test_top_1_train_top_p_-1.0_test_top_p_-1.0/outputs/3-CodeSearchNet \
-#    --per_device_train_batch_size 8 \
+#    --per_device_train_batch_size 32 \
 #    --per_device_eval_batch_size 16 \
-#    --gradient_accumulation_steps 4 \
+#    --gradient_accumulation_steps 1 \
 #    --learning_rate 3e-04 \
 #    --attn_lr 0.0 \
 #    --num_train_epochs 3 \
@@ -194,9 +194,9 @@ DS_CONFIG="configs/ds_configs/stage2_without_offload.config"
 #    --gen_data_dir generated_data/lora_gen_superni_llama \
 #    --task_config_dir configs/CodeTask/BFP \
 #    --output_dir logs_and_outputs/test_t5_codetask_train_top_1_test_top_1_train_top_p_-1.0_test_top_p_-1.0/outputs/4-BFP \
-#    --per_device_train_batch_size 8 \
+#    --per_device_train_batch_size 32 \
 #    --per_device_eval_batch_size 16 \
-#    --gradient_accumulation_steps 4 \
+#    --gradient_accumulation_steps 1 \
 #    --learning_rate 3e-04 \
 #    --attn_lr 0.0 \
 #    --num_train_epochs 3 \
@@ -261,9 +261,9 @@ python src/run_t5_new.py \
    --run_name test_t5_codetask_train_top_1_test_top_1_train_top_p_-1.0_test_top_p_-1.0 \
    --distances_temperature 1.0 \
    --distances_way L2 \
-   --max_source_length 1024 \
-   --max_target_length 512 \
-   --generation_max_length 512 \
+   --max_source_length 512 \
+   --max_target_length 300 \
+   --generation_max_length 300 \
    --add_task_name False \
    --add_dataset_name False \
    --overwrite_output_dir \
@@ -305,9 +305,9 @@ python src/run_t5_new.py \
    --gen_data_dir generated_data/lora_gen_superni_llama \
    --task_config_dir configs/CodeTask/RunBugRun \
    --output_dir logs_and_outputs/test_t5_codetask_train_top_1_test_top_1_train_top_p_-1.0_test_top_p_-1.0/outputs/6-RunBugRun \
-   --per_device_train_batch_size 8 \
+   --per_device_train_batch_size 32 \
    --per_device_eval_batch_size 16 \
-   --gradient_accumulation_steps 4 \
+   --gradient_accumulation_steps 1 \
    --learning_rate 3e-04 \
    --attn_lr 0.0 \
    --num_train_epochs 3 \
@@ -359,9 +359,9 @@ python src/run_t5_new.py \
    --gen_data_dir generated_data/lora_gen_superni_llama \
    --task_config_dir configs/CodeTask/TheVault_Csharp \
    --output_dir logs_and_outputs/test_t5_codetask_train_top_1_test_top_1_train_top_p_-1.0_test_top_p_-1.0/outputs/7-TheVault_Csharp \
-   --per_device_train_batch_size 8 \
+   --per_device_train_batch_size 32 \
    --per_device_eval_batch_size 16 \
-   --gradient_accumulation_steps 4 \
+   --gradient_accumulation_steps 1 \
    --learning_rate 3e-04 \
    --attn_lr 0.0 \
    --num_train_epochs 3 \
@@ -413,9 +413,9 @@ python src/run_t5_new.py \
    --gen_data_dir generated_data/lora_gen_superni_llama \
    --task_config_dir configs/CodeTask/CoST \
    --output_dir logs_and_outputs/test_t5_codetask_train_top_1_test_top_1_train_top_p_-1.0_test_top_p_-1.0/outputs/8-CoST \
-   --per_device_train_batch_size 8 \
+   --per_device_train_batch_size 32 \
    --per_device_eval_batch_size 16 \
-   --gradient_accumulation_steps 4 \
+   --gradient_accumulation_steps 1 \
    --learning_rate 3e-04 \
    --attn_lr 0.0 \
    --num_train_epochs 3 \
@@ -468,9 +468,9 @@ rm -rf logs_and_outputs/test_t5_codetask_train_top_1_test_top_1_train_top_p_-1.0
 #    --gen_data_dir generated_data/lora_gen_superni_llama \
 #    --task_config_dir configs/CodeTask/BFP \
 #    --output_dir logs_and_outputs/test_t5_codetask_train_top_1_test_top_1_train_top_p_-1.0_test_top_p_-1.0/outputs/15-task875_emotion_classification \
-#    --per_device_train_batch_size 8 \
+#    --per_device_train_batch_size 32 \
 #    --per_device_eval_batch_size 8 \
-#    --gradient_accumulation_steps 4 \
+#    --gradient_accumulation_steps 1 \
 #    --learning_rate 3e-04 \
 #    --attn_lr 0.0 \
 #    --num_train_epochs 3 \
--- t5_normal_full_without_instruction_pool.sh
+++ t5_normal_full_without_instruction_pool.sh
@@ -251,9 +251,9 @@ python src/run_t5_new.py \
    --gen_data_dir generated_data/lora_gen_superni_llama \
    --task_config_dir configs/CodeTask/KodCode \
    --output_dir logs_and_outputs/test_t5_codetask_train_top_1_test_top_1_train_top_p_-1.0_test_top_p_-1.0_without_instruction_pool/outputs/5-KodCode \
-   --per_device_train_batch_size 32 \
+   --per_device_train_batch_size 4 \
    --per_device_eval_batch_size 16 \
-   --gradient_accumulation_steps 1 \
+   --gradient_accumulation_steps 4 \
    --learning_rate 3e-04 \
    --attn_lr 0.0 \
    --num_train_epochs 3 \
@@ -261,9 +261,9 @@ python src/run_t5_new.py \
    --run_name test_t5_codetask_train_top_1_test_top_1_train_top_p_-1.0_test_top_p_-1.0_without_instruction_pool \
    --distances_temperature 1.0 \
    --distances_way L2 \
-   --max_source_length 1024 \
-   --max_target_length 512 \
-   --generation_max_length 512 \
+   --max_source_length 512 \
+   --max_target_length 300 \
+   --generation_max_length 300 \
    --add_task_name False \
    --add_dataset_name False \
    --overwrite_output_dir \
